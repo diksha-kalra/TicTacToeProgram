dbb8dc84e4d16f9bf738e88bf1439c2e9e019136
diksha-kalra <kalradiksha11@gmail.com>
2020-10-04 23:24:28 +0530

    Refactor : Taking user input for X or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,39 +1,39 @@
 package com.cpg.tictactoe;
+
 import java.util.*;
 
 public class TicTacToe {
-	Scanner sc=new Scanner(System.in);
-	
-	public void createBoard() {
-		//creating char array of size 10
+	Scanner sc = new Scanner(System.in);
+
+	private char[] createBoard() {
+		// creating char array of size 10
 		char[] ticTacToeBoard = new char[10];
-		//initializing array 
+		// initializing array
 		for (int i = 1; i < 10; i++) {
 			ticTacToeBoard[i] = ' ';
 		}
+		return ticTacToeBoard;
 	}
-	
-	public char chooseLetter() {
+
+	private char chooseLetter() {
 		System.out.println("Choose a Letter 'X' or 'O");
-		char userLetter=sc.next().charAt(0);
+		char userLetter = sc.next().charAt(0);
 		return Character.toUpperCase(userLetter);
-		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Progarm");
-		
-		//creating object of class TicTacToe
+		// creating object of class TicTacToe
 		TicTacToe board = new TicTacToe();
-		board.createBoard(); 
-		char userChoice =board.chooseLetter();
+		char[] ticTacToeBoard = board.createBoard();
+		// calling choose letter method
+		char userChoice = board.chooseLetter();
 		char computerChoice;
-		if(userChoice=='X') 
-			computerChoice='O';
+		if (userChoice == 'X')
+			computerChoice = 'O';
 		else
-			computerChoice='X';
-		System.out.println("Chosen letter for player is :"+userChoice);
-		System.out.println("Chosen letter for player is :"+computerChoice);
-					
+			computerChoice = 'X';
+		System.out.println("Chosen letter for player is :" + userChoice);
+		System.out.println("Chosen letter for computer is :" + computerChoice);
 	}
 }
-
